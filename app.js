03bef87e7b9d314bc9bdbf6e854027f22592cc56
fujio toshimasa <mcc41-choro@hotmail.co.jp>
2021-11-21 09:01:12 +0900

    セクション15-61　まで完了

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //memo
-//分割代入:https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+//①分割代入:https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+//②__dirname には、現在実行中のソースコードが格納されているディレクトリパスが格納されています。
 //
 const PORT = process.env.PORT;
 const express = require("express");
@@ -14,6 +15,14 @@ const app = express();
 app.set("view engine","ejs");
 //サーバサイド情報の隠ぺい
 app.disable("x-powered-by");
+
+//exports global method to view engine.
+app.use( (req,res,next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 // Static resource routing
 app.use(favicon(path.join(__dirname,"/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
@@ -23,23 +32,8 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(acceslogger());
 
 // Dynamic resource routing
+app.use("/shops",require("./routes/shops.js"));
 app.use("/",require("./routes/index.js"));
-app.use("/test", async(req,res,next) => {
-  const { MySQLClient , sql } = require("./lib/database/client.js");
-  var data;
-
-  try{
-    data = await MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"),  [1]  );
-    console.log(data);
-  }catch(err){
-    next(err);
-  }finally{
-    await MySQLClient.end();
-  }
-
-  res.end("OK");
-
-});
 
 // Set application log.
 app.use(applicationlogger());
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 router.get("/",(req,res)=>{
   //viewsフォルダからの相対パスでejsを指定
+  // res.render(path, data(オブジェクト))
+  // 関数/値を渡す res.locals.method = () => {}
   res.render("./index.ejs");
 });
 
